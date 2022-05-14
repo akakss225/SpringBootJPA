260826fb28e076c70d5dae4f1a2c7df683c50b75
akakss225 <akakss225@naver.com>
2022-05-14 15:42:11 +0900

    JDBC Connection - jdbc 표준을 이용

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/Toy_Project_SpringBoot?serverTimezone=UTC&characterEncoding=UTF-8";
+    public static final String USERNAME = "howmuch";
+    public static final String PASSWORD = "howmuch";
+
+}
