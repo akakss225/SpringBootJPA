d903fb38433bf65887b8e7d929b21e04a1cb4baa
akakss225 <akakss225@naver.com>
2022-08-29 17:33:32 +0900

    2022.08.29, 공부 재시작

--- src/main/java/hello/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -14,6 +14,7 @@ public class DBConnectionUtil {
     // jdbc 표준
     public static Connection getConnection(){
         try {
+            // ConnectionConst.URL >> (option + Enter) static import 이용해서 URL, USERNAME, PASSWORD 로 간단히 표현
             Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
             log.info("get connection={}, class={}", connection, connection.getClass());
             return connection;
--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -44,5 +44,4 @@ class MemberRepositoryV0Test {
 
     }
 
-
 }
