bb85848b35ad087bd92fc4e2a848ff8b3557d2a0
akakss225 <akakss225@naver.com>
2022-09-04 16:11:46 +0900

    커넥션풀의 기본. test

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,42 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    // 처음 객체를 생성할떄만, parameter 를 넣어주고, 이후에 사용할 때에는 이미 할당된 커넥션풀에서 가져와 사용하면 됨
+    // 연결할때마다 호출하던 기존의 JDBC 방식에서 한단계 진화됨 >> 속도 향상
+    // "설정과 사용을 분리" 했다는 관점에서 굉장히 중요한 의의가 있음
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+}
