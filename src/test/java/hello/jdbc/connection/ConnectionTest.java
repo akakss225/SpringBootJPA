a003a22916ac4a2be0bd85fda4cd1a2f7da82cea
akakss225 <akakss225@naver.com>
2022-09-04 16:50:24 +0900

    Connection Pool Test

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -37,6 +38,26 @@ public class ConnectionTest {
         Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링 >> Spring 에서 자동으로 제공해주는 커넥션풀
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        // default 가 10개임
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        // 아래 코드가 없으면, Connection Pool 에 추가되는 부분을 볼 수 없다(확인 불가).
+        Thread.sleep(1000);
+
+        // why? 별도의 thread를 사용해 커넥션풀에 커넥션을 채울까
+        // 커넥션풀에 커넥션을 채우는 것은 상대적으로 오래 걸리는 작업.
+        // 애플리케이션을 실행할 때 커넥션 풀을 채울 때 까지 대기하고 있다면, 애플리케이션 실행시간이 느려짐.
     }
 
 }
