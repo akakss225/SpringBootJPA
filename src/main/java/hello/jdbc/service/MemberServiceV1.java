a917bf1fb630fa449ef2e1e000416bc045af21c9
akakss225 <akakss225@naver.com>
2022-10-08 18:16:11 +0900

    트랜잭션 - 정상, 예외시 트랜잭션 미적용 코드

---
+++ src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,31 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.SQLException;
+
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+
+    private final MemberRepositoryV1 memberRepositoryV1;
+
+    public void accoountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        Member fromMember = memberRepositoryV1.findById(fromId);
+        Member toMember = memberRepositoryV1.findById(toId);
+
+        memberRepositoryV1.update(fromId, fromMember.getMoney() - money);
+        // 중간에 예외처리
+        validation(toMember);
+        memberRepositoryV1.update(toId, toMember.getMoney() + money);
+
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외발생");
+        }
+    }
+}
